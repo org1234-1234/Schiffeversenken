5e989b6da85139fea7f460bffcdf23c9641654b7
elxa1234 <1llal1s1r@gmail.com>
2024-02-14 08:52:02 +0100

    Add project files.

---
+++ Schiffeversenken/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schiffeversenken
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string multiLineText = "";
+            for (int i = 1; i <= 10; i++)
+            {
+                multiLineText += $"Zeile {i}\n";
+            }
+            textBox1.Text = multiLineText;
+
+            textBox1.BackColor = Color.LightGray;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
